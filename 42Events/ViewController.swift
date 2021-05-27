dd5301dd2f9199a598f8ad1bb03c7780d6dad3be
Tuan Diep <baotuan.nt@gmail.com>
2021-05-27 10:46:32 +0700

    Initial Commit

---
+++ 42Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  42Events
+//
+//  Created by Tuan Diep on 5/27/21.
+//  Copyright © 2021 Tuan Diep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
